9e90c7684c90839bddc5781cd1fd5ad42054331a
yuuddin <jindf51@naver.com>
2024-04-05 17:53:34 +0900

    [CHORE] 프로젝트 기본 세팅

---
+++ src/main/java/backend/newssseuk/NewsSseukApplication.java
@@ -0,0 +1,13 @@
+package backend.newssseuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSseukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSseukApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/newssseuk/NewsSseukApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.newssseuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSseukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
