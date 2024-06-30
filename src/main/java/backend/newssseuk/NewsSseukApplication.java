063fb19d4fd1de87b65634f3adb08226c3e35987
이가현 <igahyeon@igahyeon-ui-MacBookPro.local>
2024-06-30 18:05:25 +0900

    기사 크롤링 수정

--- src/main/java/backend/newssseuk/NewsSseukApplication.java
+++ src/main/java/backend/newssseuk/NewsSseukApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsSseukApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NewsSseukApplication.class, args);
     }
--- src/main/java/backend/newssseuk/springbootmongodb/ArticleController.java
+++ src/main/java/backend/newssseuk/springbootmongodb/ArticleController.java
@@ -1,9 +1,7 @@
 package backend.newssseuk.springbootmongodb;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,8 +10,7 @@ import java.util.List;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ArticleController {
     private final CrawlingService crawlingService;
-
-    @GetMapping("api/crawling")
+    @GetMapping("/api/crawling")
     public List<Article> crawling(){
         System.out.println("컨트롤러 실행");
         return crawlingService.getCrawlingInfos();
