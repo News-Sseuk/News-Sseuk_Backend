1555b5e84a4f7357f9f90689880238b05f4e12c3
이가현 <igahyeon@igahyeon-ui-MacBookPro.local>
2024-09-06 03:34:12 +0900

    [FEATURE] 카테고리별로 스크랩 기사 불러오기 (no-offset pagination 적용)

--- src/main/java/backend/newssseuk/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/backend/newssseuk/domain/scrap/repository/ScrapRepository.java
@@ -3,5 +3,5 @@ package backend.newssseuk.domain.scrap.repository;
 import backend.newssseuk.domain.scrap.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+public interface ScrapRepository extends JpaRepository<Scrap, Long>, ScrapRepositoryCustom {
 }
