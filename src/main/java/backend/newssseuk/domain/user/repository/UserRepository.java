ed93d9092d391ddcb353982b7ed2a83792c38a44
yuuddin <hyj032564@gmail.com>
2024-10-11 00:21:55 +0900

    관심 카테고리 get api 수정

--- src/main/java/backend/newssseuk/domain/user/repository/UserRepository.java
+++ src/main/java/backend/newssseuk/domain/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package backend.newssseuk.domain.user.repository;
 
+import backend.newssseuk.domain.enums.Category;
 import backend.newssseuk.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
@@ -12,4 +16,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findById(Long userId);
+
+    @Query("SELECT u.interestedCategory FROM User u WHERE u.id = :userId")
+    Set<Category> findInterestedCategoriesByUserId(@Param("userId") Long userId);
 }
